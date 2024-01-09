65ff798ac980118c6d264468228df605131a7e4e
XenioxYT <tomlockwood135@outlook.com>
2024-01-09 01:12:18 +0000

    Add ConfigurationSettingForm and save configuration settings to .env file

--- jarvisSetup/webserver/views.py
+++ jarvisSetup/webserver/views.py
@@ -1,8 +1,10 @@
 # Create your views here.
 from django.shortcuts import render, redirect
-from .forms import GeneralCustomizationForm, ToolsForm
-from .models import GeneralCustomization, Tools
+from .forms import GeneralCustomizationForm, ToolsForm, ConfigurationSettingForm
+from .models import GeneralCustomization, Tools, ConfigurationSetting
+from dotenv import load_dotenv, dotenv_values, set_key
 from django.contrib import messages
+from pathlib import Path
 
 def home(request):
     return render(request, 'home.html')
@@ -32,11 +34,54 @@ def tools(request):
 
 
 def configuration_settings(request):
+    tool_settings = Tools.objects.first()
+
+    if request.method == 'POST':
+        # Manually extract the values from the POST data
+        keys = [
+            'picovoice_access_key', 'openai_api_key', 'openai_api_base', 'discord_token',
+            'google_api_key', 'google_cse_id', 'google_maps_api_key', 'home_assistant_token',
+            'home_assistant_url'
+        ]
+
+        # Fetch or create a single ConfigurationSetting instance
+        config, _ = ConfigurationSetting.objects.get_or_create(id=1)
+
+        variables = {}
+
+        for key in keys:
+            value = request.POST.get(key)
+            if value not in ['None', None, '']:
+                setattr(config, key, value)
+                variables[key] = value
+
+        config.save()
+
+        with open('.env', 'w') as file:
+            for var, value in variables.items():
+                if value not in ['None', None]:
+                    file.write(f'{var}={value}\n')
+
+        # Save the changes
+        try:
+            config.save()
+        except Exception as e:
+            messages.error(request, f'Error saving configuration: {e}')
+            return redirect('configuration_settings')
+
+        messages.success(request, 'Configuration updated successfully!')
+        return redirect('configuration_settings')
+    
+    # If GET request, prepare the context
     try:
-        tool_settings = Tools.objects.first()  # Retrieve the first Tools settings from the database
-    except Tools.DoesNotExist:
-        tool_settings = None
-    return render(request, 'configuration_settings.html', {'tool_settings': tool_settings})
+        current_config = ConfigurationSetting.objects.get(id=1)
+    except ConfigurationSetting.DoesNotExist:
+        current_config = None
+    context = {
+        'tool_settings': tool_settings,
+        'current_config': current_config,
+    }
+    return render(request, 'configuration_settings.html', context)
 
 
 def general_customization(request):
